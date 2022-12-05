7ad0cb082ac22f163d576aed71a2fafe9e14590f
retkiewi <58555777+retkiewi@users.noreply.github.com>
2022-12-05 11:40:08 +0100

    create main.py shell

--- main.py
+++ main.py
@@ -1,2 +1,10 @@
-if __name__ == '__main__':
+import sys
+import argparse
+
+def main(args):
     pass
+
+if __name__ == '__main__':
+    parser  = argparse.ArgumentParser()
+    # add your args here
+    main(parser.parse_args(sys.argv[1:]))
