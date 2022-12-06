a2bc45858a9f7de898015bdb54a1cb01ac88f5dc
retkiewi <krzret@student.agh.edu.pl>
2022-12-06 18:44:31 +0100

    p1 done

--- main.py
+++ main.py
@@ -2,14 +2,16 @@ import sys
 import argparse
 import networkx as nx
 from productions.p1 import P1
+import matplotlib.pyplot as plt
 
 def main(args):
     G = nx.Graph()
-    G.add_node(1, label='El')
+    G.add_node(1, node_color='red', label='El', pos=(0, 0))
+
     P1.apply(G)
 
-    nx.draw(G)
-
+    nx.draw(G, nx.get_node_attributes(G,'pos'), labels=nx.get_node_attributes(G,'label'), font_size=10)
+    plt.show()
 
 if __name__ == '__main__':
     parser  = argparse.ArgumentParser()
--- productions/p1.py
+++ productions/p1.py
@@ -14,18 +14,29 @@ class P1():
 
         if not El_node: return False
 
-        El_node[1]['label']
+        El_node[1]['label'] = 'el'
         (pos_x, pos_y) = El_node[1]['pos']
-        G.add_node(El_node[0]+1, label='E', pos=(pos_x-offset/2, pos_y-offset/2))
+        G.add_node(El_node[0]+1, label='I', pos=(pos_x, pos_y-offset))
         G.add_edge(El_node[0], El_node[0]+1)
+
+        I_node = (El_node[0]+1, G.nodes[El_node[0]+1])
+        (pos_x, pos_y) = I_node[1]['pos']
+
+        G.add_node(I_node[0]+1, label='E', pos=(pos_x-offset/2, pos_y-offset/2))
+        G.add_edge(I_node[0], I_node[0]+1)
         
-        G.add_node(El_node[0]+2, label='E', pos=(pos_x+offset/2, pos_y-offset/2))
-        G.add_edge(El_node[0], El_node[0]+2)
+        G.add_node(I_node[0]+2, label='E', pos=(pos_x+offset/2, pos_y-offset/2))
+        G.add_edge(I_node[0], I_node[0]+2)
         
-        G.add_node(El_node[0]+3, label='E', pos=(pos_x-offset/2, pos_y+offset/2))
-        G.add_edge(El_node[0], El_node[0]+3)
+        G.add_node(I_node[0]+3, label='E', pos=(pos_x+offset/2, pos_y+offset/2))
+        G.add_edge(I_node[0], I_node[0]+3)
         
-        G.add_node(El_node[0]+4, label='E', pos=(pos_x+offset/2, pos_y+offset/2))
-        G.add_edge(El_node[0], El_node[0]+4)
+        G.add_node(I_node[0]+4, label='E', pos=(pos_x-offset/2, pos_y+offset/2))
+        G.add_edge(I_node[0], I_node[0]+4)
+
+        G.add_edge(I_node[0]+1, I_node[0]+2)
+        G.add_edge(I_node[0]+2, I_node[0]+3)
+        G.add_edge(I_node[0]+3, I_node[0]+4)
+        G.add_edge(I_node[0]+4, I_node[0]+1)
 
         return True
