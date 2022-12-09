8aee2850e8f16a269a47236dc5810b807d662c52
retkiewi <krzret@student.agh.edu.pl>
2022-12-09 10:01:52 +0100

    remove unnecessary offset

--- main.py
+++ main.py
@@ -8,7 +8,7 @@ def main(args):
     G = nx.Graph()
     G.add_node(1, node_color='red', label='El', pos=(0, 0))
 
-    P1.apply(G, offset=15)
+    P1.apply(G)
 
     nx.draw(G, nx.get_node_attributes(G,'pos'), labels=nx.get_node_attributes(G,'label'), font_size=10)
     plt.show()
