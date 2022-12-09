564916af7b36428172d434d2783633b696514944
retkiewi <krzret@student.agh.edu.pl>
2022-12-09 10:04:08 +0100

    style all files

--- main.py
+++ main.py
@@ -4,16 +4,19 @@ import networkx as nx
 from productions.p1 import P1
 import matplotlib.pyplot as plt
 
+
 def main(args):
     G = nx.Graph()
     G.add_node(1, node_color='red', label='El', pos=(0, 0))
 
     P1.apply(G)
 
-    nx.draw(G, nx.get_node_attributes(G,'pos'), labels=nx.get_node_attributes(G,'label'), font_size=10)
+    nx.draw(G, nx.get_node_attributes(G, 'pos'),
+            labels=nx.get_node_attributes(G, 'label'), font_size=10)
     plt.show()
 
+
 if __name__ == '__main__':
-    parser  = argparse.ArgumentParser()
+    parser = argparse.ArgumentParser()
     # add your args here
     main(parser.parse_args(sys.argv[1:]))
--- productions/p1.py
+++ productions/p1.py
@@ -2,6 +2,7 @@ import networkx as nx
 from productions.decorators import first_isomorphism
 from typing import Dict
 
+
 class P1():
     left = nx.Graph()
     left.add_node(1, label='El')
@@ -9,8 +10,9 @@ class P1():
     @staticmethod
     @first_isomorphism(left)
     def apply(G: nx.Graph, offset: int = 1, isomorphism: Dict = None):
-        if isomorphism is None:return False
-        
+        if isomorphism is None:
+            return False
+
         El_node_id = list(isomorphism.keys())[0]
         El_node = (El_node_id, G.nodes[El_node_id])
 
@@ -22,16 +24,20 @@ class P1():
         I_node = (El_node[0]+1, G.nodes[El_node[0]+1])
         (pos_x, pos_y) = I_node[1]['pos']
 
-        G.add_node(I_node[0]+1, label='E', pos=(pos_x-offset/2, pos_y-offset/2))
+        G.add_node(I_node[0]+1, label='E',
+                   pos=(pos_x-offset/2, pos_y-offset/2))
         G.add_edge(I_node[0], I_node[0]+1)
-        
-        G.add_node(I_node[0]+2, label='E', pos=(pos_x+offset/2, pos_y-offset/2))
+
+        G.add_node(I_node[0]+2, label='E',
+                   pos=(pos_x+offset/2, pos_y-offset/2))
         G.add_edge(I_node[0], I_node[0]+2)
-        
-        G.add_node(I_node[0]+3, label='E', pos=(pos_x+offset/2, pos_y+offset/2))
+
+        G.add_node(I_node[0]+3, label='E',
+                   pos=(pos_x+offset/2, pos_y+offset/2))
         G.add_edge(I_node[0], I_node[0]+3)
-        
-        G.add_node(I_node[0]+4, label='E', pos=(pos_x-offset/2, pos_y+offset/2))
+
+        G.add_node(I_node[0]+4, label='E',
+                   pos=(pos_x-offset/2, pos_y+offset/2))
         G.add_edge(I_node[0], I_node[0]+4)
 
         G.add_edge(I_node[0]+1, I_node[0]+2)
