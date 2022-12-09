3857c3ef5a97ac3f788d6841dc7510bbdccdeed1
retkiewi <krzret@student.agh.edu.pl>
2022-12-09 10:06:21 +0100

    Revert "style all files", merge pr first
    This reverts commit 564916af7b36428172d434d2783633b696514944.

--- productions/utils.py
+++ productions/utils.py
@@ -2,11 +2,9 @@ from typing import Dict, List
 from networkx import Graph
 from networkx.algorithms.isomorphism import GraphMatcher
 
-
 def find_isomorphisms(G_main: Graph, G_to_find: Graph) -> List[Dict]:
-    GM = GraphMatcher(G_main, G_to_find, node_match=lambda n1,
-                      n2: n1['label'] == n2['label'])
-
+    GM = GraphMatcher(G_main, G_to_find, node_match=lambda n1,n2: n1['label']==n2['label'])
+    
     filtered_isomorphisms = []
     for isomorphism in GM.subgraph_isomorphisms_iter():
         if all([isomorphism.keys() != filtered_isomorphism.keys() for filtered_isomorphism in filtered_isomorphisms]):
