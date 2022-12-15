4fe9808ca60214542c3ea48be7049f50e4cde61b
retkiewi <58555777+retkiewi@users.noreply.github.com>
2022-12-15 21:52:38 +0100

    Add testcase to p1 tests, add test for p1, p2 flow (#14)

--- tests/test_p1.py
+++ tests/test_p1.py
@@ -1,7 +1,4 @@
-import matplotlib.pyplot as plt
 import networkx as nx
-import pytest
-
 from productions import P1
 
 
@@ -17,5 +14,18 @@ def test_p1_should_be_applied():
             (4, {'label': 'E', 'pos': (1, 0), 'layer': 1}),
             (5, {'label': 'E', 'pos': (0, 1), 'layer': 1}),
             (6, {'label': 'E', 'pos': (1, 1), 'layer': 1})] == list(G.nodes(data=True))
-    
-    assert {(1, 2), (2, 3), (2, 4), (2, 5), (2, 6), (3, 4), (3, 5), (4, 6), (5, 6)} == set(G.edges)
+
+    assert {(1, 2), (2, 3), (2, 4), (2, 5), (2, 6), (3, 4),
+            (3, 5), (4, 6), (5, 6)} == set(G.edges)
+
+
+def test_p1_cant_be_applied():
+    G = nx.Graph()
+    G.add_node(1, label='el', pos=(1/2, 1/2), layer=0)
+
+    assert False == P1.apply(G)
+
+    assert [(1, {'label': 'el', 'pos': (0.5, 0.5), 'layer': 0})
+            ] == list(G.nodes(data=True))
+
+    assert set() == set(G.edges)
