e1d8e40c3dd81f021cd7040231581ea842f056ef
michal367 <michal.krawczyk7@gmail.com>
2022-12-17 19:07:08 +0100

    review fixes

--- visualization.py
+++ visualization.py
@@ -8,7 +8,7 @@ def map_label_to_color(label):
     return color_map.get(label.lower(), '#c1c1c1')
 
 
-def map_pos_to_vis_pos(pos, layer):
+def map_pos_to_visual_pos(pos, layer):
     x_size = 10
     y_size = 10
     y_offset = 6
@@ -28,17 +28,19 @@ def get_layer_subgraph(graph: nx.Graph, layer: int):
     return graph.subgraph(layer_nodes)
 
 
-def update_labels_with_duplicated_pos(labels, positions):
+def relabel_labels_with_duplicated_pos(labels, positions):
+    labels2 = labels.copy()
     pos_label_dict = {}
     for id, pos in positions.items():
         if pos in pos_label_dict:
-            pos_label_dict[pos] += labels[id]
+            pos_label_dict[pos] += labels2[id]
         else:
-            pos_label_dict[pos] = labels[id]
+            pos_label_dict[pos] = labels2[id]
 
     for id, pos in positions.items():
-        labels[id] = pos_label_dict[pos]
+        labels2[id] = pos_label_dict[pos]
 
+    return labels2
 
 def draw_graph(graph: nx.Graph, layer=None):
     if layer is not None:
@@ -47,15 +49,14 @@ def draw_graph(graph: nx.Graph, layer=None):
     labels = nx.get_node_attributes(graph, 'label')
     node_colors = list(map(map_label_to_color, labels.values()))
 
-    vis_pos = nx.get_node_attributes(graph, 'pos')
+    visual_pos = nx.get_node_attributes(graph, 'pos')
     if layer is None:
-        vis_pos = {node: map_pos_to_vis_pos(
+        visual_pos = {node: map_pos_to_visual_pos(
             data['pos'], data['layer']) for node, data in graph.nodes(data=True)}
 
-    update_labels_with_duplicated_pos(labels, vis_pos)
+    labels = get_labels_with_duplicated_pos(labels, visual_pos)
 
-
-    nx.draw(graph, pos=vis_pos,
+    nx.draw(graph, pos=visual_pos,
             labels=labels, font_size=12,
             node_size=300, node_color=node_colors)
     plt.show()
